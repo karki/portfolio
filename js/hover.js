d2e711dca434856a965db337ac856ef0526ab757
Utu Kärki <utu.karki@proton.me>
2023-02-15 13:38:44 +0200

    Add multiple images and change them on click

--- js/hover.js
+++ js/hover.js
@@ -3,29 +3,55 @@ import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
 import { WavesPass } from "./WavesPass";
 
-var img = document.getElementById("texture");
+let currentVisibleImageIndex = 0;
+const loader = new THREE.TextureLoader();
 
-var texture;
-var plane;
 var uMouseVelocity = new THREE.Vector2(0, 0);
 
-var camera;
-var renderer;
-var composer;
-var scene;
+const textures = document.getElementsByClassName("texture");
+const imageMeshes = [];
+
+for (let i = 0; i < textures.length; i++) {
+  const imageElement = textures[i];
+
+  loader.load(imageElement.src, function (texture) {
+    texture.needsUpdate = true;
+
+    imageMeshes.push(
+      createImagePlane(
+        imageElement.clientWidth,
+        imageElement.clientHeight,
+        texture
+      )
+    );
+
+    // Only init after all images on the page have been loaded.
+    if (imageMeshes.length == textures.length) {
+      init();
+      animate();
+    }
+  });
+}
+
+function createImagePlane(width, height, texture) {
+  const geometry = new THREE.PlaneGeometry(1, 1);
+  const material = new THREE.MeshBasicMaterial({
+    map: texture,
+    side: THREE.DoubleSide,
+  });
+  const plane = new THREE.Mesh(geometry, material);
+  plane.scale.set(width, height, 1);
+  plane.visible = false;
+
+  return plane;
+}
+
+let camera;
+let renderer;
+let composer;
+let scene;
 
 let wavesPass;
-
-var imageElement = document.createElement("img");
-imageElement.onload = function (e) {
-  texture = new THREE.Texture(this);
-  texture.needsUpdate = true;
-  init();
-  animate();
-  img.style.display = "none";
-};
-
-imageElement.src = img.src;
 
 function init(w, h) {
   renderer = new THREE.WebGLRenderer();
@@ -44,14 +70,9 @@ function init(w, h) {
 
   scene = new THREE.Scene();
 
-  const geometry = new THREE.PlaneGeometry(1, 1);
-  const material = new THREE.MeshBasicMaterial({
-    map: texture,
-    side: THREE.DoubleSide,
-  });
-  plane = new THREE.Mesh(geometry, material);
-  plane.scale.set(img.clientWidth, img.clientHeight, 1);
-  scene.add(plane);
+  imageMeshes[currentVisibleImageIndex].visible = true;
+
+  imageMeshes.forEach((mesh) => scene.add(mesh));
 
   composer = new EffectComposer(renderer);
   const renderPass = new RenderPass(scene, camera);
@@ -66,6 +87,7 @@ function init(w, h) {
 
 function animate() {
   requestAnimationFrame(animate);
+
   composer.render();
 }
 
@@ -81,19 +103,47 @@ window.addEventListener("resize", () => {
   const width = window.innerWidth;
   const height = window.innerHeight;
 
-  camera.left = window.innerWidth / -2,
-  camera.right = window.innerWidth / 2,
-  camera.top = window.innerHeight / 2,
-  camera.bottom = window.innerHeight / -2,
-
-  camera.aspect = width / height;
-
+  (camera.left = window.innerWidth / -2),
+    (camera.right = window.innerWidth / 2),
+    (camera.top = window.innerHeight / 2),
+    (camera.bottom = window.innerHeight / -2),
+    (camera.aspect = width / height);
   camera.updateProjectionMatrix();
 
-  const oldRendererSize = new THREE.Vector2(0, 0);
-  renderer.getSize(oldRendererSize);
-
-  wavesPass.material.uniforms["uResolution"].value = new THREE.Vector2(width, height);
+  wavesPass.material.uniforms["uResolution"].value = new THREE.Vector2(
+    width,
+    height
+  );
 
   renderer.setSize(width, height);
+});
+
+// onClick raycasting things
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+window.addEventListener("click", (event) => {
+  // calculate pointer position in normalized device coordinates
+  // (-1 to +1) for both components
+
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // update the picking ray with the camera and pointer position
+  raycaster.setFromCamera(pointer, camera);
+
+  // calculate objects intersecting the picking ray
+  const intersects = raycaster.intersectObjects(scene.children);
+
+  if (intersects.length > 0) {
+    imageMeshes[currentVisibleImageIndex].visible = false;
+
+    if (currentVisibleImageIndex == imageMeshes.length - 1) {
+      currentVisibleImageIndex = 0;
+    } else {
+      currentVisibleImageIndex++;
+    }
+
+    imageMeshes[currentVisibleImageIndex].visible = true;
+  }
 });
